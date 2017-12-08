ce27be21cf82a3878c2d17e36c92a9eaad93c704
naufik <nauffik@gmail.com>
2017-12-08 19:52:10 +1100

    polished day5 py solution

--- day5/day5_maze.py
+++ day5/day5_maze.py
@@ -7,6 +7,7 @@
 '''
 
 def maze_exit(instructions):
+	''' Finds the path to exit the instruction set given. Notice that '''
 	path = [0]
 	while (0 <= path[-1] < len(instructions)):
 		path.append(path[-1] + instructions[path[-1]])
@@ -14,7 +15,10 @@ def maze_exit(instructions):
 
 	return path
 
+# read and parse the testcase file
 text = open("testcase").read()
 sequence = [int(k) for k in text.split('\n') if k != '']
 
+# print how many 'jumps' are required to finish exit the instruction set
+# note that the 'start' line is excluded.
 print(len(maze_exit(sequence)) - 1)
